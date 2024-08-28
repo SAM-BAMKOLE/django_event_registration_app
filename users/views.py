bfbeb0e82226df7840f1bc5878376d304abde933
samBamkole <olagoroyesamuel7@gmail.com>
2024-08-28 15:06:48 +0100

    Prevented access to login and register routes when user logged in

--- users/views.py
+++ users/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, get_user, logout
 from .models import RegisterForm
+from  django.contrib.auth.models import AnonymousUser
 
 # Create your views here.
 def register(request: HttpRequest):
     form = RegisterForm()
+
+    user = get_user(request)
 
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -14,19 +17,28 @@ def register(request: HttpRequest):
         if form.is_valid():
             login(request, form.save())
             return redirect(to='home_page')
-    return render(request, 'register.html', { "form": form })
+        
+    if not isinstance(user, AnonymousUser):
+        return redirect("home_page")
+    
+    return render(request, 'register.html', { "form": form, "user": user })
 
 
 def login_view(request: HttpRequest):
+    user = get_user(request)
+
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         # validate form
         if form.is_valid():
             login(request, form.get_user())
             return redirect('home_page')
+        
+    if not isinstance(user, AnonymousUser):
+        return redirect("home_page")
+    
     form = AuthenticationForm()
-
-    return render(request, 'signin.html', { "form": form })
+    return render(request, 'signin.html', { "form": form, "user": user })
 
 
 def logout_view(request):
